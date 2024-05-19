f80128923ffa24435d37259f421926685963417d
xiphocoleon <thomasedwardwilk@gmail.com>
2024-05-19 13:50:29 -0400

    Manual commit

--- PY_Convert_BMP_to_Other_Format.py
+++ PY_Convert_BMP_to_Other_Format.py
@@ -2,16 +2,26 @@ from PIL import Image
 import glob
 import os
 
+#Dimensions of resized images
+dimX = 300
+dimY = 300
+
 #Path to BMP images:
-path_to_bmps = 'C:\\Users\\thoma\\Pictures\\Geodis\\2979R21-lane1\\*.BMP'
+root_dir = 'C:\\Users\\thoma\\Pictures\\Geodis\\3473SRP-lane 2 fails'
+path_to_bmps = root_dir + '\\*.BMP'
 
 #Path to converted images:
-path_to_reformatted_images = 'C:\\Users\\thoma\\Pictures\\Geodis\\2979R21-lane1\\png'
+path_to_reformatted_images = root_dir + '\\png'
 os.mkdir(path_to_reformatted_images)
 
-cnt = 0
-for img in glob.glob(path_to_bmps):
-    Image.open(img).resize((300,300)).save(os.path.join(path_to_reformatted_images, str(cnt) + '.png'))
-    cnt += 1
 
+def convert_BMP_to_PNG():
+    cnt = 0
+    for img in glob.glob(path_to_bmps):
+        Image.open(img).resize((dimX,dimY)).save(os.path.join(path_to_reformatted_images, str(cnt) + '.png'))
+        cnt += 1
 
+def copy_images_in_PNG_folder_to_all_images_folder():
+    #To be developed later
+    dummy = 0
+    
